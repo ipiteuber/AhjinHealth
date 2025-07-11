8de9dc6d9a3d74614edddb3f9d3af4e512412d80
ipiteuber <140553021+ipiteuber@users.noreply.github.com>
2025-07-11 16:42:25 -0400

    Actualizacion home page

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioService, Usuario } from 'src/app/services/usuario/usuario.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
-  standalone: false
+  standalone: false,
 })
 export class HomePage implements OnInit {
+  usuario: Usuario | null = null;
 
-  constructor() { }
+  constructor(private usuarioService: UsuarioService) {}
 
   ngOnInit() {
+    this.usuario = this.usuarioService.getUsuarioLocal();
   }
-
 }
